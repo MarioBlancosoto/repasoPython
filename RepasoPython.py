d8860a4cef1875d2e7fc695d90879e922fbbfbe5
mblancosoto <986882601>
2017-10-16 14:40:54 +0200

    repaso

--- RepasoPython.py
+++ RepasoPython.py
@@ -1,31 +1,85 @@
-#1
-def multiplos(n):
+import random
+def comparaLista(lista,k):
 
-    for multiplo in range(1,11):
-        print(multiplo*n)
+    iguais = []
+    maiores = []
+    menores = []
 
-
-multiplos(4);
-print("-----------------")
-#2
-def indefinido():
-
-    for indef in range(10,21):
-        print(indef)
-
-
-indefinido()
+    for elemento in lista:
+     if elemento==k:
+         iguais.append(elemento)
+     elif elemento>k:
+         maiores.append(elemento)
+     else:
+         menores.append(elemento)
 
 
-print("-----------------")
+    return maiores,iguais,menores
 
-#3
+listaExemplo = [1,2,3,4,5,6,7,7,7,8,9,10]
 
-def temp(far):
 
-    for num in range(0,121,10):
+l1,l2,l3 = comparaLista(listaExemplo,7)
 
-     celsius = (num-32)*(5/9)
-     print(celsius)
+print(l1)
+print(l2)
+print(l3)
 
-temp(100)
+
+#exercicio 14
+
+def contaPalabras(cadea):
+    palabras = cadea.split()
+    diccionario = dict()
+    for palabra in palabras:
+        if not palabra in diccionario:
+            repeticions=0
+            for elemento in palabras:
+                if elemento == palabra:
+                    repeticions = repeticions+1
+            diccionario[palabra] = repeticions
+
+    return diccionario
+
+
+def contaPalabras2(cadea):
+    palabras = cadea.split()
+    diccionario = dict()
+    for palabra in palabras:
+        if palabra in diccionario:
+           diccionario[palabra]= diccionario[palabra]+1
+        else:
+         diccionario[palabra] = 1
+
+    return diccionario
+
+
+print (contaPalabras("Hola que tal amigos,que imos facer hoxe?"))
+print(contaPalabras2("Hola que tal amigos,que imos facer hoxe?"))
+
+
+
+
+#15
+def tiradasDados(veces):
+    tiradas = list()
+    for tirada in range(veces+1):
+     tirada1 = random.randrange(1,6)
+     tirada2 = random.randrange(1,6)
+     tiradas.append((tirada1,tirada2))
+    return tiradas
+
+def contaValorTiradas(tiradasDados):
+    cuentaTiradas = dict()
+    for tirada in tiradasDados:
+        valor = tirada[0]+tirada[1]
+        if valor in cuentaTiradas:
+          cuentaTiradas[valor] = cuentaTiradas[valor]+1
+        else:
+            cuentaTiradas[valor]=1
+
+    return cuentaTiradas
+
+
+
+print(contaValorTiradas(tiradasDados(5)))
